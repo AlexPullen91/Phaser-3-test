ed25f363770c4d39469fd55fb9ef942beec241d1
AlexPullen91 <alex_pullen03@hotmail.com>
2020-05-10 17:28:25 +0000

    Added code for animations and sound effects

--- js/SceneMain.js
+++ js/SceneMain.js
@@ -2,7 +2,7 @@ class SceneMain extends Phaser.Scene {
     constructor() {
         super({ key: "SceneMain" });
     }
-
+// loads in images and animations
     preload() {
         this.load.image("sprBg0", "content/sprBg0.png");
         this.load.image("sprBg1", "content/sprBg1.png");
@@ -25,10 +25,49 @@ class SceneMain extends Phaser.Scene {
             frameWidth: 16,
             frameHeight: 16
         });
+// loads in audio/sfx
+        this.load.audio("sndExplode0", "content/sndExplode0.wav");
+        this.load.audio("sndExplode1", "content/sndExplode1.wav");
+        this.load.audio("sndLaser", "content/sndLaser.wav");
     }
 
     
     create() {
+        this.anims.create({
+            key: "sprEnemy0",
+            frames: this.anims.generateFrameNumbers("sprEnemy0"),
+            frameRate: 20,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: "sprEnemy2",
+            frames: this.anims.generateFrameNumbers("sprEnemy2"),
+            frameRate: 20,
+            repeat: -1
+        });
+        
+        this.anims.create({
+            key: "sprExplosion",
+            frames: this.anims.generateFrameNumbers("sprExplosion"),
+            frameRate: 20,
+            repeat: 0
+        });
+
+        this.anims.create({
+            key: "sprPlayer",
+            frames: this.anims.generateFrameNumbers("sprPlayer"),
+            frameRate: 20,
+            repeat: -1
+        });
+
+        this.sfx = {
+            explosions: [
+                this.sound.add("sndExplode0"),
+                this.sound.add("sndExplode1")
+            ],
+            laser: this.sound.add("sndLaser")
+        };
 
     }
 };
